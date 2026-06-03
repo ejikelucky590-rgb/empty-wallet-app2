0a612af2b28272308366f3800387c4954545dcbf
ejikelucky590-rgb <ejikelucky590-rgb@users.noreply.github.com>
2026-06-03 10:12:19 +0100

    fix: import go_router context extensions in onboarding screen

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:flutter/services.dart';
 
 import '../models/country_model.dart';
